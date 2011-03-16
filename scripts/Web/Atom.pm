8588c056a11c3cd45fc3fc450c1e2cb454110cb0
Gea-Suan Lin <darkkiller@gmail.com>
2011-03-16 10:41:20 +0800

    Use Atom 1.0.

--- scripts/Web/Atom.pm
+++ scripts/Web/Atom.pm
@@ -17,26 +17,26 @@ sub _build_feed {
 
     my $plugin = $self->plugin;
 
-    my $author = XML::Atom::Person->new;
+    my $author = XML::Atom::Person->new(Version => 1.0);
     $author->email($plugin->author_email);
     $author->name($plugin->author_name);
 
-    my $link = XML::Atom::Link->new;
+    my $link = XML::Atom::Link->new(Version => 1.0);
     $link->type('text/html');
     $link->rel('alternative');
     $link->href($plugin->url);
 
-    my $feed = XML::Atom::Feed->new;
+    my $feed = XML::Atom::Feed->new(Version => 1.0);
     $feed->add_link($link);
     $feed->author($author);
     $feed->id($plugin->id);
     $feed->title($plugin->title);
 
     foreach my $e (@{$plugin->entries}) {
-	my $entry = XML::Atom::Entry->new;
+	my $entry = XML::Atom::Entry->new(Version => 1.0);
 
 	if (defined $entry->author) {
-	    my $entryAuthor = XML::Atom::Author->new;
+	    my $entryAuthor = XML::Atom::Author->new(Version => 1.0);
 	    $entryAuthor->email($e->author_email);
 	    $entryAuthor->name($e->author_name);
 	    $entry->author($entryAuthor);
@@ -46,7 +46,7 @@ sub _build_feed {
 
 	$entry->content($e->content);
 
-	my $link = XML::Atom::Link->new;
+	my $link = XML::Atom::Link->new(Version => 1.0);
 	$link->type('text/html');
 	$link->rel('alternative');
 	$link->href($e->url);
