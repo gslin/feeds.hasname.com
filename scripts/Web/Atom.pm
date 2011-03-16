431ca4eac4fba9a1e59c179a1df7d4f589b2a937
Gea-Suan Lin <darkkiller@gmail.com>
2011-03-16 09:10:59 +0800

    Import new framework Web::Atom to handle.

---
+++ scripts/Web/Atom.pm
@@ -0,0 +1,74 @@
+package Web::Atom;
+use strict;
+use warnings;
+
+use Any::Moose;
+has 'feed' => (is => 'rw', isa => 'XML::Atom::Feed', lazy => 1);
+has 'p' => (is => 'ro', isa => 'Str', required => 1);
+has 'plugin' => (is => 'rw', isa => 'Web::Atom::Plugin', lazy => 1);
+
+use XML::Atom::Entry;
+use XML::Atom::Feed;
+use XML::Atom::Link;
+use XML::Atom::Person;
+
+sub build_feed {
+    my $self = shift;
+
+    my $plugin = $self->plugin;
+
+    my $author = XML::Atom::Person->new;
+    $author->email($plugin->author_email);
+    $author->name($plugin->author_name);
+
+    my $link = XML::Atom::Link->new;
+    $link->type('text/html');
+    $link->rel('alternative');
+    $link->href($plugin->url);
+
+    my $feed = XML::Atom::Feed->new;
+    $feed->add_link($link);
+    $feed->author($author);
+    $feed->id($plugin->id);
+    $feed->title($plugin->title);
+
+    foreach my $e (@$plugin->entries) {
+	my $entry = XML::Atom::Entry->new;
+
+	if (defined $entry->author) {
+	    my $entryAuthor = XML::Atom::Author->new;
+	    $entryAuthor->email($e->author_email);
+	    $entryAuthor->name($e->author_name);
+	    $entry->author($entryAuthor);
+	} else {
+	    $entry->author($author);
+	}
+
+	$entry->content($e->content);
+
+	my $link = XML::Atom::Link->new;
+	$link->type('text/html');
+	$link->rel('alternative');
+	$link->href($e->url);
+	$entry->add_link($link);
+
+	$feed->add_entry($entry);
+    }
+
+    return $feed;
+}
+
+sub build_plugin {
+    my $self = shift;
+
+    my $p = $self->p;
+    my $pname = "Web::Atom::$p";
+
+    require $pname;
+    my $plugin;
+    eval "\$plugin = $pname->new;";
+
+    return $plugin;
+}
+
+1;
---
+++ scripts/Web/Atom/Plugin.pm
@@ -0,0 +1,7 @@
+package Web::Atom::Plugin;
+use strict;
+use warnings;
+
+use Class::Accessor qw/url/;
+
+1;
