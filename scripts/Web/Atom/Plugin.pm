206622fe351a9002e16b080436e49813ab52a5f4
Gea-Suan Lin <darkkiller@gmail.com>
2011-03-16 09:21:53 +0800

    Add some default value to base class.

--- scripts/Web/Atom/Plugin.pm
+++ scripts/Web/Atom/Plugin.pm
@@ -3,10 +3,10 @@ use strict;
 use warnings;
 
 use Any::Moose;
-has 'author_email' => (is => 'rw', isa => 'Str');
-has 'author_name' => (is => 'rw', isa => 'Str');
+has 'author_email' => (is => 'rw', isa => 'Str', default => 'example@example.com');
+has 'author_name' => (is => 'rw', isa => 'Str', default => 'John Doe');
 has 'id' => (is => 'rw', isa => 'Str');
-has 'title' => (is => 'rw', isa => 'Str');
+has 'title' => (is => 'rw', isa => 'Str', default => 'Default title');
 has 'url' => (is => 'rw', isa => 'Str');
 
 use Carp;
