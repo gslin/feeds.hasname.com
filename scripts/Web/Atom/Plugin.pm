8fae11f5031a69257cb48e27b34cdb00103d07de
Gea-Suan Lin <darkkiller@gmail.com>
2011-03-16 09:18:38 +0800

    Use Any::Moose.

--- scripts/Web/Atom/Plugin.pm
+++ scripts/Web/Atom/Plugin.pm
@@ -2,6 +2,17 @@ package Web::Atom::Plugin;
 use strict;
 use warnings;
 
-use Class::Accessor qw/url/;
+use Any::Moose;
+has 'author_email' => (is => 'rw', isa => 'Str');
+has 'author_name' => (is => 'rw', isa => 'Str');
+has 'id' => (is => 'rw', isa => 'Str');
+has 'title' => (is => 'rw', isa => 'Str');
+has 'url' => (is => 'rw', isa => 'Str');
+
+use Carp;
+
+sub entries {
+    croak 'Not implemented';
+}
 
 1;
