27fa8bcf8227ea0bf26a915223337a1d398b44a0
Gea-Suan Lin <darkkiller@gmail.com>
2011-03-16 11:00:49 +0800

    Remove debug code...

--- scripts/cathaybk.creditcard.pl
+++ scripts/cathaybk.creditcard.pl
@@ -10,9 +10,6 @@ main();
 sub main {
     my $feed = Web::Atom->new(p => 'Bank::Cathaybk::Creditcard');
 
-    print $feed->as_xml;
-    return;
-
     open FEED, '> /var/www/feeds.hasname.com/webroot/feed/cathaybk.creditcard.atom' or croak $!;
     print FEED $feed->as_xml;
     close FEED;
