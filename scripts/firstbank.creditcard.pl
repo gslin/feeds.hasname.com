28dd4f270c46e0775fd978998cdc86eed841ace2
Gea-Suan Lin <darkkiller@gmail.com>
2011-03-18 00:06:28 +0800

    Add firstbank script.

---
+++ scripts/firstbank.creditcard.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/env perl
+
+use Carp;
+use Web::Atom;
+use strict;
+use warnings;
+
+main();
+
+sub main {
+    my $feed = Web::Atom->new(p => 'Bank::Firstbank::Creditcard');
+    $feed->id('http://feeds.hasname.com/feed/firstbank.creditcard.atom');
+
+    open FEED, '> /var/www/feeds.hasname.com/webroot/feed/firstbank.creditcard.atom' or croak $!;
+    print FEED $feed->as_xml;
+    close FEED;
+}
+
+__END__
