8e559c2bbdf6428a9a763a7a1acb615960a25961
Gea-Suan Lin <darkkiller@gmail.com>
2011-03-20 07:38:00 +0800

    Use INIT to replace origin main().

--- scripts/cosmosbank.creditbank.pl
+++ scripts/cosmosbank.creditbank.pl
@@ -5,9 +5,7 @@ use Web::Atom;
 use strict;
 use warnings;
 
-main();
-
-sub main {
+INIT {
     my $feed = Web::Atom->new(p => 'Bank::Cosmosbank::Creditcard');
     $feed->id('http://feeds.hasname.com/feed/cosmosbank.creditcard.atom');
 
